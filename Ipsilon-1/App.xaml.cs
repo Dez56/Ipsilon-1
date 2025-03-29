709f989964eb4d963fc87f8e0510320a250a336b
ELEAZAR CARRRANZA QUINTERO <eleazar.carrranzaq@uanl.edu.mx>
2025-03-29 17:13:56 -0600

    Add project files.

---
+++ Ipsilon-1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ipsilon_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ipsilon-1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ipsilon_1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
