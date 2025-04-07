c9b7618f49028b79b819b3f0d80393219b4b9ac9
Dez56 <ezasitocarranza@gmail.com>
2025-04-06 20:21:07 -0600

    Se pueden hacer consultas ahora

--- Ipsilon-1/Views/movint/Consultas.xaml.cs
+++ Ipsilon-1/Views/movint/Consultas.xaml.cs
@@ -15,7 +15,23 @@ public partial class Consultas : ContentPage
         
     }
 
-    private async void Load(object sender, EventArgs e)
+    private async void nigga1click (object sender, EventArgs e)
+    {
+        uno.IsVisible = true;
+        tua.IsVisible = false;
+        await Load();
+    }
+
+    private async void nigga2click (object sender, EventArgs e)
+    {
+        tua.IsVisible = true;
+        uno.IsVisible = false;
+        await leed();
+    }
+
+    //Los siguientes metodos son consultas a las bases de datos, que llenan el modelo, esto se aprecia en la 4ta linea util de ambos metodos
+
+    private async Task Load()
     {
         string url = "http://localhost:5015/Usuarios"; 
         using (HttpClient client = new HttpClient())
@@ -27,11 +43,34 @@ public partial class Consultas : ContentPage
 
         }
     }
+
+    private async Task leed()
+    {
+        string url = "http://localhost:5015/Paquetes";
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetStringAsync(url);
+
+            var data = JsonConvert.DeserializeObject<List<Paquete>>(response);
+            sasas.ItemsSource = data;
+
+        }
+    }
 }
+
+//modelos usados para llenar los grids
 
 public class Usario
 {
     public required int Id { get; set; }
     public required string Nombre { get; set; }
     public required string Contrasena { get; set; }
+}
+
+public class Paquete
+{
+    public int Id { get; set; }
+    public int Repártidor { get; set; }  //esta linea en todas sus formas en algun momento se va a joder toda la aplicacion, anota eso Jimmy
+    public required string Codigo { get; set; }
+    public int Estado { get; set; } // "0 = En proceso de entrega", "1 = Entregado", "Cancelado"
 }
